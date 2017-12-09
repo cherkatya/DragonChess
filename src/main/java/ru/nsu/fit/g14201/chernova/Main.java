c0f588ab15b165a495247442afb029e6522e94b1
cherkatya <katyushechka.chernova@yandex.ru>
2017-12-09 11:10:34 +0700

    initial commit

---
+++ src/main/java/ru/nsu/fit/g14201/chernova/Main.java
@@ -0,0 +1,10 @@
+package ru.nsu.fit.g14201.chernova;
+
+/**
+ * Created by Катя on 07.12.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(0);
+    }
+}

ace9fa363d8ff17d4460bd05ed2da34f2f634ca0
cherkatya <katyushechka.chernova@yandex.ru>
2017-12-21 10:29:13 +0700

    Added abstract class Figure

---
+++ src/main/java/ru/nsu/fit/g14201/chernova/model/Figure.java
@@ -0,0 +1,16 @@
+package ru.nsu.fit.g14201.chernova.model;
+
+import java.util.List;
+
+/**
+ * Created by Катя on 21.12.2017.
+ */
+public abstract class Figure {
+    private Team team;
+
+    public Team getTeam(){
+        return team;
+    }
+    public abstract List<FieldCoordinate> getPossibleMoves(Field field, FieldCoordinate coords);
+    public abstract List<FieldCoordinate> getCaptureMoves(Field field, FieldCoordinate coords);
+}
---
+++ src/main/java/ru/nsu/fit/g14201/chernova/model/Team.java
@@ -0,0 +1,7 @@
+package ru.nsu.fit.g14201.chernova.model;
+
+/**
+ * Created by Катя on 21.12.2017.
+ */
+public class Team {
+}

968274c92616a9f6d6372f2ef92c93e8056d128e
cherkatya <katyushechka.chernova@yandex.ru>
2017-12-21 10:17:04 +0700

    FieldCoordinate

---
+++ src/main/java/ru/nsu/fit/g14201/chernova/model/FieldCoordinate.java
@@ -0,0 +1,22 @@
+package ru.nsu.fit.g14201.chernova.model;
+
+/**
+ * Created by Катя on 21.12.2017.
+ */
+public class FieldCoordinate {
+    private int board;
+    private int x;
+    private int y;
+
+    public int getBoard() {
+        return board;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}

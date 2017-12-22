a4b837a29775705fb8daee1a4dcccfb3ed33c802
lipatkin96 <lipatkin96@gmail.com>
2017-12-23 01:10:59 +0700

    Draw board at another color

--- src/main/java/ru/nsu/fit/g14201/chernova/Board.java
+++ src/main/java/ru/nsu/fit/g14201/chernova/Board.java
@@ -7,17 +7,25 @@ import java.awt.*;
  * Created by SPN on 23.12.2017.
  */
 public class Board extends JPanel {
+    private Color firstSideColor = new Color(0xffd700);
+    private Color secondSideColor = new Color(220, 20, 60);
+
     public void paint(Graphics g){
+        g.setColor(firstSideColor);
         g.fillRect(100, 100, 400, 400);
+
+        g.setColor(secondSideColor);
         for(int i = 100; i <= 400; i+=100){
             for(int j = 100; j <= 400; j+=100){
-                g.clearRect(i, j, 50, 50);
+                //g.clearRect(i, j, 50, 50);
+                g.fillRect(i, j, 50, 50);
             }
         }
 
         for(int i = 150; i <= 450; i+=100){
             for(int j = 150; j <= 450; j+=100){
-                g.clearRect(i, j, 50, 50);
+                //g.clearRect(i, j, 50, 50);
+                g.fillRect(i, j, 50, 50);
             }
         }
     }
--- src/main/java/ru/nsu/fit/g14201/chernova/ChessFrame.java
+++ src/main/java/ru/nsu/fit/g14201/chernova/ChessFrame.java
@@ -3,6 +3,8 @@ package ru.nsu.fit.g14201.chernova;
 /**
  * Created by SPN on 23.12.2017.
  */
+import org.w3c.dom.css.RGBColor;
+
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JFrame;
@@ -10,13 +12,15 @@ import javax.swing.JPanel;
 
 public class ChessFrame extends JFrame {
 
+    private Color backgroundColor = Color.LIGHT_GRAY;
+
     public ChessFrame() {
         super("Dragon Chess");
 
         setSize(600,600);
         getContentPane().add(new Board());
         setLocationRelativeTo(null);
-        setBackground(Color.LIGHT_GRAY);
+        setBackground(backgroundColor);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }

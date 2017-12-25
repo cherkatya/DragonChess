01e8ee1a49a3ac3395ba0961c321366743e2e730
kvinster <fornewreal@gmail.com>
2017-12-25 20:29:58 +0700

    implemented Hero

--- src/main/java/ru/nsu/fit/g14201/chernova/model/figures/Hero.java
+++ src/main/java/ru/nsu/fit/g14201/chernova/model/figures/Hero.java
@@ -12,18 +12,64 @@ import java.util.List;
  * Created by Катя on 21.12.2017.
  */
 public class Hero extends Figure {
+    private FieldCoordinate lastPosition;
+
     public Hero(Team team){
         super(team);
+        lastPosition = null;
+    }
+
+    private List<FieldCoordinate> getMoves(Field field, FieldCoordinate coords){
+        if ((coords.getBoard() != 1) && (lastPosition != null))
+        {
+            throw new IllegalArgumentException("Hero appeared on the edge board by illegal means");
+        }
+
+        List<FieldCoordinate> moves = new ArrayList<>();
+
+        switch (coords.getBoard())
+        {
+            case 0:
+            case 2:
+            {
+                moves.add(lastPosition);
+                break;
+            }
+            case 1:
+            {
+                lastPosition = coords;
+
+                for (int i = 1; i <= 2; i++)
+                {
+                    moves.add(new FieldCoordinate(1, coords.getX() - i, coords.getY() - i));
+                    moves.add(new FieldCoordinate(1, coords.getX() - i, coords.getY() + i));
+                    moves.add(new FieldCoordinate(1, coords.getX() + i, coords.getY() - i));
+                    moves.add(new FieldCoordinate(1, coords.getX() + i, coords.getY() + i));
+                }
+
+                checkMoveValidity(moves, field, team);
+
+                break;
+            }
+            default:
+                throw new IllegalArgumentException("Illegal board number");
+        }
+
+        return moves;
     }
 
     public List<FieldCoordinate> getPossibleMoves(Field field, FieldCoordinate coords){
-        List<FieldCoordinate> possibleMoves = new ArrayList<>();
+        List<FieldCoordinate> moves = getMoves(field, coords);
 
-        return possibleMoves;
+        checkPossibilityMoves(moves, field);
+
+        return moves;
     }
     public List<FieldCoordinate> getCaptureMoves(Field field, FieldCoordinate coords){
-        List<FieldCoordinate> captureMoves = new ArrayList<>();
+        List<FieldCoordinate> moves = getMoves(field, coords);
 
-        return captureMoves;
+        checkCaptureMoves(moves, field);
+
+        return moves;
     }
 }

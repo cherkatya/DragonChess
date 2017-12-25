0a33c9b83e7b7555a5935371560004047afd97c1
kvinster <fornewreal@gmail.com>
2017-12-26 05:27:52 +0700

    halfway through selectNewCell

--- src/main/java/ru/nsu/fit/g14201/chernova/model/GameModel.java
+++ src/main/java/ru/nsu/fit/g14201/chernova/model/GameModel.java
@@ -1,9 +1,16 @@
 package ru.nsu.fit.g14201.chernova.model;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class GameModel {
     private Field field;
 
     public GameModel(Field field) {
         this.field = field;
+    }
+
+    public boolean isCheckState(Team toTeam)
+    {
+        throw new NotImplementedException();
     }
 }
--- src/main/java/ru/nsu/fit/g14201/chernova/presenter/Presenter.java
+++ src/main/java/ru/nsu/fit/g14201/chernova/presenter/Presenter.java
@@ -1,12 +1,13 @@
 package ru.nsu.fit.g14201.chernova.presenter;
 
 import org.apache.log4j.Logger;
-import ru.nsu.fit.g14201.chernova.model.Field;
-import ru.nsu.fit.g14201.chernova.model.FieldCoordinate;
-import ru.nsu.fit.g14201.chernova.model.Figure;
-import ru.nsu.fit.g14201.chernova.model.GameModel;
+import ru.nsu.fit.g14201.chernova.model.*;
 import ru.nsu.fit.g14201.chernova.view.ChessDragonView;
 import ru.nsu.fit.g14201.chernova.view.FieldCoordinateView;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by castiel on 26.12.2017.
@@ -15,12 +16,16 @@ public class Presenter {
     private static Logger log = Logger.getLogger(Presenter.class);
 
     private GameModel model;
+    private PlayerInfo playerInfo;
     private Field field;
     private FieldCoordinate selectedCell;
+    private List<FieldCoordinate> currentPossibleMoves;
+    private List<FieldCoordinate> currentCaptureMoves;
 
     public Presenter() {
         field = new Field();
         model = new GameModel(field);
+        playerInfo = new PlayerInfo();
         selectedCell = null;
     }
 
@@ -36,7 +41,7 @@ public class Presenter {
 
     public void selectCell(FieldCoordinateView cellCoord) {
         log.debug("Coord = " + cellCoord.getBoard() + " " + cellCoord.getX() + " " + cellCoord.getY());
-        FieldCoordinate fc = new FieldCoordinate(cellCoord.getBoard(), cellCoord.getX(), cellCoord.getY()); //TODO make sure those are right oriented
+        FieldCoordinate fc = new FieldCoordinate(cellCoord.getBoard(), cellCoord.getX(), cellCoord.getY());
 
         if (selectedCell == null)
         {
@@ -54,11 +59,25 @@ public class Presenter {
             return;
 
         Figure figure = field.getFigure(newCell);
+        if (figure.getTeam() != playerInfo.getCurrentTurn())
+            return;
 
+        selectedCell = newCell;
+
+        currentPossibleMoves = figure.getPossibleMoves(field, newCell);
+        currentPossibleMoves.removeIf(coordinate -> !isValidMove(new GameMove(newCell, coordinate)));
+
+        currentCaptureMoves = figure.getCaptureMoves(field, newCell);
+    }
+
+    private boolean isValidMove(GameMove move)
+    {
+        throw new NotImplementedException(); //TODO implement
     }
 
     public void start()
     {
         field.populate();
+        playerInfo.startGame();
     }
 }

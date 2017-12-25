e81d4ec43edc6c749bccf550166c2c20d9711571
kvinster <fornewreal@gmail.com>
2017-12-26 04:30:53 +0700

    created GameModel

--- src/main/java/ru/nsu/fit/g14201/chernova/presenter/Presenter.java
+++ src/main/java/ru/nsu/fit/g14201/chernova/presenter/Presenter.java
@@ -1,6 +1,9 @@
 package ru.nsu.fit.g14201.chernova.presenter;
 
 import org.apache.log4j.Logger;
+import ru.nsu.fit.g14201.chernova.model.Field;
+import ru.nsu.fit.g14201.chernova.model.FieldCoordinate;
+import ru.nsu.fit.g14201.chernova.model.GameModel;
 import ru.nsu.fit.g14201.chernova.view.ChessDragonView;
 import ru.nsu.fit.g14201.chernova.view.FieldCoordinateView;
 
@@ -9,6 +12,16 @@ import ru.nsu.fit.g14201.chernova.view.FieldCoordinateView;
  */
 public class Presenter {
     private static Logger log = Logger.getLogger(Presenter.class);
+
+    private GameModel model;
+    private Field field;
+    private FieldCoordinate selectedCell;
+
+    public Presenter() {
+        field = new Field();
+        model = new GameModel(field);
+        selectedCell = null;
+    }
 
     public ChessDragonView getView() {
         return view;
@@ -20,8 +33,12 @@ public class Presenter {
 
     private ChessDragonView view;
 
-
     public void selectCell(FieldCoordinateView cellCoord) {
         log.debug("Coord = " + cellCoord.getBoard() + " " + cellCoord.getX() + " " + cellCoord.getY());
+    }
+
+    public void start()
+    {
+        field.populate();
     }
 }
